7a3d32abc0f25d02c249fbdd582e9088b6c49125
sajjad <sajjadsajjad098765@gmail.com>
2024-10-18 01:00:25 +0600

    order product display

--- src/Pages/PlaceOrder.jsx
+++ src/Pages/PlaceOrder.jsx
@@ -1,10 +1,137 @@
+import CartTotal from "../Components/CartTotal";
+import Title from "../Components/Title";
+import paymentLogo from "../assets/stripe.png";
+import rezorpay from "../assets/rezorpay.png";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const PlaceOrder = () => {
-    return (
-        <div>
-            PlaceOrder
+  const [method, setMethod] = useState("cod");
+  const navigate = useNavigate();
+
+  return (
+    <div className="flex flex-col sm:flex-row justify-between gap-4 pt-5 sm:pt-14 min-h-[80vh] border-t px-4 sm:px-8">
+      {/* --------------Left Side------------- */}
+      <div className="flex flex-col gap-6 w-full sm:max-w-[500px]">
+        <div className="text-xl sm:text-2xl my-3">
+          <Title text1={"DELIVERY"} text2={"INFORMATION"} />
         </div>
-    );
+
+        <div className="flex flex-col gap-4">
+          <div className="flex flex-col sm:flex-row gap-4">
+            <input
+              type="text"
+              placeholder="First Name"
+              className="border border-gray-300 rounded py-2 px-4 w-full focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+            <input
+              type="text"
+              placeholder="Last Name"
+              className="border border-gray-300 rounded py-2 px-4 w-full focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+          </div>
+
+          <input
+            type="email"
+            placeholder="Enter your email"
+            className="border border-gray-300 rounded py-2 px-4 w-full focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
+
+          <input
+            type="text"
+            placeholder="Street"
+            className="border border-gray-300 rounded py-2 px-4 w-full focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
+
+          <div className="flex flex-col sm:flex-row gap-4">
+            <input
+              type="text"
+              placeholder="City"
+              className="border border-gray-300 rounded py-2 px-4 w-full focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+            <input
+              type="text"
+              placeholder="State"
+              className="border border-gray-300 rounded py-2 px-4 w-full focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+          </div>
+
+          <div className="flex flex-col sm:flex-row gap-4">
+            <input
+              type="number"
+              placeholder="Zip code"
+              className="border border-gray-300 rounded py-2 px-4 w-full focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+            <input
+              type="text"
+              placeholder="Country"
+              className="border border-gray-300 rounded py-2 px-4 w-full focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+          </div>
+
+          <input
+            type="number"
+            placeholder="Phone"
+            className="border border-gray-300 rounded py-2 px-4 w-full focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
+        </div>
+      </div>
+
+      {/* ---------------Right Side------------------- */}
+      <div className="mt-8">
+        <div className="mt-8 min-w-80">
+          <CartTotal />
+        </div>
+        <div className="mt-12">
+          <Title text1={"PAYMENT"} text2={"METHOD"} />
+          <div className="flex gap-3 flex-col lg:flex-row"></div>
+          {/* -----------Payment Method------------------- */}
+          <div className="flex gap-3 flex-col lg:flex-row">
+            <div
+              onClick={() => setMethod("stripe")}
+              className="flex items-center gap-3 border p-2 px-3 cursor-pointer"
+            >
+              <p
+                className={`min-w-3.5 h-3.5 border rounded-full ${
+                  method === "stripe" ? "bg-green-600" : ""
+                }`}
+              ></p>
+              <img src={paymentLogo} alt="" className="h-10 mx-4" />
+            </div>
+            <div
+              onClick={() => setMethod("rezorpay")}
+              className="flex items-center gap-3 border p-2 px-3 cursor-pointer"
+            >
+              <p
+                className={`min-w-3.5 h-3.5 border rounded-full ${
+                  method === "rezorpay" ? "bg-green-600" : ""
+                }`}
+              ></p>
+              <img src={rezorpay} alt="" className="h-10 mx-4" />
+            </div>
+            <div
+              onClick={() => setMethod("cod")}
+              className="flex items-center gap-3 border p-2 px-3 cursor-pointer"
+            >
+              <p
+                className={`min-w-3.5 h-3.5 border rounded-full ${
+                  method === "cod" ? "bg-green-600" : ""
+                }`}
+              ></p>
+              <p className="text-gray-500 text-sm font-medium mx-4">
+                CASH ON DELIVERY
+              </p>
+            </div>
+          </div>
+          <div className="w-full text-end mt-8">
+            <button onClick={()=> navigate('/orders')} className="bg-black text-white px-16 py-3 text-sm">
+              PLACE ORDER
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PlaceOrder;
